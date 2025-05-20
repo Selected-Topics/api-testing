f20e78ff021cb3361b76a39b2f3514c303fa0ae4
zelalem61 <zelalemhab19@gmail.com>
2025-05-20 23:39:40 +0300

    add mongo configuration in the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ApiTokenGuard } from './auth/guards/api-token.guard';
 import { AuthenticationGuard } from './auth/guards/authentication.guard';
 import { JwtGuard } from './auth/guards/jwt.guard';
 import jwtConfig from './config/jwt.config';
+import mongoConfig from './config/mongo.config';
 import { HealthModule } from './health/health.module';
 import { OrderModule } from './order/order.module';
 import { ProductModule } from './product/product.module';
@@ -19,13 +20,17 @@ import { ProductModule } from './product/product.module';
     HealthModule,
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [jwtConfig],
+      load: [jwtConfig, mongoConfig],
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGO_URI'),
-      }),
+      useFactory: (configService: ConfigService) => {
+        const mongoConfig = configService.get<{ mongoUri: string }>('mongo');
+        const mongoUri = mongoConfig?.mongoUri;
+        return {
+          uri: mongoUri,
+        };
+      },
       inject: [ConfigService],
     }),
   ],
