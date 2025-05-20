1f846861a70460f9fa1dba8857c8c9643fa48c6b
zelalem61 <zelalemhab19@gmail.com>
2025-05-20 23:39:53 +0300

    add a default mongo uri

--- src/config/mongo.config.ts
+++ src/config/mongo.config.ts
@@ -2,5 +2,5 @@ import { registerAs } from '@nestjs/config';
 import { CONFIG_TOKEN } from './config.token';
 
 export default registerAs(CONFIG_TOKEN.MONGO, () => ({
-  mongoUri: process.env.MONGO_URI,
+  mongoUri: process.env.MONGO_URI ?? 'mongodb://localhost:27017/api-testing',
 }));
